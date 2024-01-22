92318232d7b98aebe38d54dfbadcb054ec4316a4
Paul ARNAUD <paularnaud2@gmail.com>
2024-01-22 17:04:39 +0100

    json save & restore

--- Resize/main.py
+++ Resize/main.py
@@ -1,5 +1,17 @@
 import time
+import json
+import parutils as u
+from typing import List
 import pygetwindow as gw
+from pygetwindow import Win32Window
+
+TITLES = [
+    'UBS Workspace',
+    'Google Chrome',
+    'Visual Studio Code',
+    'C:\\',
+]
+DATA_PATH = 'Resize/data.json'
 
 
 def get_infos():
@@ -20,12 +32,44 @@ def resize(title, width, hight, left, top):
     window.moveTo(left, top)
 
 
+def save_to_json():
+    windows: List[Win32Window] = [w for w in gw.getAllWindows() if u.like_list(w.title, TITLES)]
+    d = {}
+    for w in windows:
+        d[w.title] = {
+            'width': w.width,
+            'height': w.height,
+            'left': w.left,
+            'top': w.top,
+        }
+    u.log_dict(d)
+    u.save_list([json.dumps(d)], DATA_PATH)
+
+
+def restore_json():
+    d = json.loads(u.load_txt(DATA_PATH, False))
+    for title in d:
+        t = u.like_list(title, TITLES)
+        move_and_resize(t, d[title])
+
+
+def move_and_resize(title, d):
+    print(f"Moving and resizing '{title}'")
+    wl = gw.getWindowsWithTitle(title)
+    if not wl:
+        return
+
+    window: Win32Window = wl[0]
+    window.resizeTo(d['width'], d['height'])
+    window.moveTo(d['left'], d['top'])
+
+
 def resize_all():
-    resize('UBS Workspace - Desktop Viewer', 2488, 1399, 2737, 0)
+    resize('UBS Workspace', 2488, 1399, 2737, 0)
     resize('Word', 2391, 1399, 362, 0)
     resize('Excel', 2391, 1399, 362, 0)
-    resize('- Google Chrome', 2391, 1400, 362, 0)
-    resize('- Visual Studio Code', 2376, 1392, 369, 0)
+    resize('Google Chrome', 2391, 1400, 362, 0)
+    resize('Visual Studio Code', 2376, 1392, 369, 0)
 
     # window = gw.getWindowsWithTitle('- Visual Studio Code')[0]
     # window.minimize()
@@ -44,6 +88,8 @@ def show_workspace():
 
 
 if __name__ == '__main__':
-    get_infos()
+    # get_infos()
     # resize_all()
     # show_workspace()
+    # save_to_json()
+    restore_json()
--- Resize/resize.py
+++ Resize/resize.py
@@ -1,4 +1,9 @@
+import os
 import main
+import os.path as p
 
 if __name__ == '__main__':
-    main.resize_all()
+    cwd = p.dirname(p.dirname(p.realpath(__file__)))
+    print(cwd)
+    os.chdir(cwd)
+    main.restore_json()
